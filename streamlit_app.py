8bcb2ca57785da39737f66938f8c822eaec7f538
Giuseppe Ferrari <35732497+ibeppo993@users.noreply.github.com>
2021-06-17 10:52:35 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,7 +13,7 @@ st.title('Predict Google Trends')
 st.text('Get Google Trends data for keywords')
 
 
-
+period = 16
 ip = get('https://api.ipify.org').text
 ip
 
@@ -59,7 +59,7 @@ def main():
             m = Prophet(weekly_seasonality=True)
             m.fit(data_predict)
 
-            future = m.make_future_dataframe(periods=16, freq='W')
+            future = m.make_future_dataframe(periods=period, freq='W')
             future.tail()
             forecast = m.predict(future)
             forecast[['ds', 'yhat', 'yhat_lower', 'yhat_upper']].tail()
@@ -76,7 +76,7 @@ def main():
                 x=alt.X('date'),
                 y=alt.Y(f'{user_input}')
             ).properties(title="Trend Forecast")
-            st.altair_chart(chart_prophet, use_container_width=True)
+            #st.altair_chart(chart_prophet, use_container_width=True)
 
             full_df = pd.merge(df_prophet, data_graph, left_on='date', right_on='date', how='left')#.drop('id1', axis=1)
             #full_df
@@ -84,6 +84,55 @@ def main():
             a = alt.Chart(full_df).mark_area(opacity=0.6).encode(x='date', y=f'{user_input}_x')
             b = alt.Chart(full_df).mark_area(opacity=1).encode(x='date', y=f'{user_input}_y')
             c = alt.layer(a, b).properties(title="Forecast and Trend Comparison")
+            st.altair_chart(c, use_container_width=True)
+
+
+            #
+            #
+            #
+            #Data test
+            data_predict = data.copy()
+            data_predict.reset_index(inplace=True)
+            #st.text('data_predict_1')
+            data_predict.drop(index=data_predict.index[-period:], axis=0, inplace=True)
+            #st.text('data_predict_2')
+            #data_predict
+
+            data_predict = data_predict.rename(columns={'date': 'ds'})
+            data_predict = data_predict.rename(columns={f'{user_input}': 'y'})
+            #data_predict.columns = ['ds', 'y']
+            #data_predict
+
+            m = Prophet(weekly_seasonality=True)
+            m.fit(data_predict)
+
+            future = m.make_future_dataframe(periods=period, freq='W')
+            future.tail()
+            forecast = m.predict(future)
+            forecast[['ds', 'yhat', 'yhat_lower', 'yhat_upper']].tail()
+            df_prophet = forecast[['ds', 'yhat']]
+
+
+            #st.text('data_prophet')
+            df_prophet = df_prophet.rename(columns={'ds': 'date'})
+            df_prophet = df_prophet.rename(columns={'yhat': f'{user_input}'})
+            #st.text('data_predict_3')
+            #df_prophet
+            print(df_prophet.info())
+
+            chart_prophet = alt.Chart(df_prophet).mark_line().encode(
+                x=alt.X('date'),
+                y=alt.Y(f'{user_input}')
+            ).properties(title="Trend Forecast")
+            #st.altair_chart(chart_prophet, use_container_width=True)
+
+            full_df = pd.merge(df_prophet, data_graph, left_on='date', right_on='date', how='left')#.drop('id1', axis=1)
+            #full_df
+
+            a = alt.Chart(full_df).mark_area(opacity=0.6).encode(x='date', y=f'{user_input}_x')
+            b = alt.Chart(full_df).mark_area(opacity=1).encode(x='date', y=f'{user_input}_y')
+            c = alt.layer(a, b).properties(title="Forecast and Trend test")
+            st.text('-----PREDICTION TEST-----')
             st.altair_chart(c, use_container_width=True)
 
 
